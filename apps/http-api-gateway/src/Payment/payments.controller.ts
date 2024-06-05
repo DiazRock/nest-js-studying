bc1096a56b1145112db488abbaf8d5e9fd9b3665
DiazRock <corolariodiaz@gmail.com>
2024-06-05 14:27:12 +0300

    Added the logger

--- apps/http-api-gateway/src/Payment/payments.controller.ts
+++ apps/http-api-gateway/src/Payment/payments.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Inject, Post, Body } from '@nestjs/common';
+import { Controller, Inject, Post, Body, Logger, HttpCode } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreatePaymentDto } from './dto/CreatePayment.dto';
 
 @Controller('payments')
 export class PaymentsController {
+  private readonly logger = new Logger(PaymentsController.name);
   constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {}
 
   @Post()
-  createPayment(@Body() createPaymentDto: CreatePaymentDto) {
-    this.natsClient.emit('createPayment', createPaymentDto);
+  @HttpCode(204)
+  async createPayment(@Body() createPaymentDto: CreatePaymentDto) {
+    this.logger.log('Creating a payment', createPaymentDto);
+    await this.natsClient.emit('createPayment', createPaymentDto);
+    return 'Payment created successfully';
   }
 }
