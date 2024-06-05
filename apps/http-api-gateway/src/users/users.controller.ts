0d1c7c987bb1c89fd3b6c9b64914f5a0543777ea
DiazRock <corolariodiaz@gmail.com>
2024-06-05 12:21:02 +0300

    Changed the controller

--- apps/http-api-gateway/src/users/users.controller.ts
+++ apps/http-api-gateway/src/users/users.controller.ts
@@ -5,28 +5,33 @@ import {
     Body,
     Get,
     Param,
-    HttpException
+    HttpException,
+    HttpCode
   } from '@nestjs/common';
   import { ClientProxy } from '@nestjs/microservices';
   import { CreateUserDto } from './dtos/CreateUser.dto';
   import { lastValueFrom } from 'rxjs';
+  import { Logger } from '@nestjs/common';
   
   @Controller('users')
   export class UsersController {
+    private readonly logger = new Logger(UsersController.name);
     constructor(@Inject('NATS_SERVICE') private natsClient: ClientProxy) {}
   
     @Post()
+    @HttpCode(204)
     createUser(@Body() createUserDto: CreateUserDto) {
-      console.log(createUserDto);
-      return this.natsClient.send({ cmd: 'createUser' }, createUserDto);
+      this.logger.log('Creating user ', createUserDto);
+      this.natsClient.send({ cmd: 'createUser' }, createUserDto);
+      return 'New user created successfully';
     }
   
     @Get(':id')
-    async getUserById(@Param('id') id: string) {
+    async getUserById(@Param('id') id: string): Promise <CreateUserDto | HttpException> {
       const user = await lastValueFrom(
         this.natsClient.send({ cmd: 'getUserById' }, { userId: id }),
       );
       if (user) return user;
-      else throw new HttpException('User Not Found', 404);
+      throw new HttpException('User Not Found', 404);
     }
   }
