ab489963a7483905ff9f62fca33980df85bc12ab
DiazRock <corolariodiaz@gmail.com>
2024-07-16 10:27:03 +0300

    Added the CreatePayment Component

--- apps/frontend/src/components/CreatePayment.jsx
+++ apps/frontend/src/components/CreatePayment.jsx
@@ -1,29 +1,37 @@
 // src/components/CreatePayment.js
 import React, { useState } from 'react';
 import { createPayment } from '../services/apiService';
+import { Form } from './Form';
+import { Input } from './Input';
 import '../styles/Form.css';
+import { num_validation, name_validation } from '../utils/inputValidations';
 
 const CreatePayment = () => {
   const [userId, setUserId] = useState('');
   const [amount, setAmount] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e, {amount, userId}) => {
+    e.preventDefault()
     try {
-      await createPayment({ amount: amount, userId: userId  });
-      alert('Payment created successfully!');
+      await createPayment({ amount, userId  });
+      return true;
     } catch (error) {
-      alert('Failed to create payment');
+      console.error('Failed to create payment', error);
+      return false
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Create Payment</h2>
-      <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="User ID" />
-      <input type="text" value={amount} onChange={(e) => setAmount(Number(e.target.value))} placeholder="Amount" />
-      <button type="submit">Create Payment</button>
-    </form>
+    <Form
+          submitCallBack={handleSubmit}
+          successText="Payment created successfully"
+          listOfInputs={ [num_validation, name_validation] }
+    />
+      /* <h2>Create Payment</h2>
+      <Input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="User ID" />
+      <Input type="text" value={amount} onChange={(e) => setAmount(Number(e.target.value))} placeholder="Amount" />
+      <button type="submit">Create Payment</button> */
+    
   );
 };
 
