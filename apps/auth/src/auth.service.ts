ae3676bd5a1dca9de9a3ff2883f49ba4b6f51be5
DiazRock <corolariodiaz@gmail.com>
2024-07-16 10:26:15 +0300

    Added auth service

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,36 +1,18 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from './users/users.service';
-import { User } from './users/schemas/user.schema';
+import jwtDecode from "jwt-decode";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtToken } from "./model/jwt-token.model";
 
 @Injectable()
 export class AuthService {
-  private readonly logger = new Logger(AuthService.name);
-  constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService,
-  ) {}
 
-  async login(user: User) {
-    if (this.usersService.verifyLogin(user.username, user.password))
-      return {
-        access_token: this.jwtService.sign(user),
-      };
-    
-  }
-
-  async register(username: string, password: string) {
-    this.logger.log("Inside the register function in the service ", username);
-    let user: User;
-    if (!this.usersService.isUserRegistered(username)) 
-      user = await this.usersService.addUser(username, password);
-    if (user === null)
-      return {answer: "The user is already logged", user: user};
-    return {answer: "User registered", user: user}
-  }
-
-  async verifyUser(jwt: string) {
-    this.logger.log("Verifying the user ", jwt);
-    return {answ: "Token verified", payload: this.jwtService.verify(jwt)};
+  public decodeJwtToken(authorizationToken: string): JwtToken {
+    let decodedToken: JwtToken;
+    try {
+      decodedToken = jwtDecode(authorizationToken);
+    }
+    catch (err) {
+      throw new UnauthorizedException();
+    }
+    return decodedToken;
   }
 }
