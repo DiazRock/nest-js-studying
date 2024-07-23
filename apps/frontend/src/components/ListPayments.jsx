f7663b9eb17cffcd0a67367cb265824152683851
DiazRock <corolariodiaz@gmail.com>
2024-07-24 00:56:50 +0300

    Added a JWT

--- apps/frontend/src/components/ListPayments.jsx
+++ apps/frontend/src/components/ListPayments.jsx
@@ -1,16 +1,18 @@
 // src/components/ListPayments.js
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { listPayments } from '../services/apiService';
 import '../styles/List.css';
 
 const ListPayments = () => {
   const [payments, setPayments] = useState([]);
+  const jwtToken = useSelector((state) => state.loginReducer.jwtSession);
 
   useEffect(() => {
     const fetchPayments = async () => {
       try {
         console.log("Listing the payments ");
-        const response = await listPayments();
+        const response = await listPayments(jwtToken);
         console.log("The response ", response);
         setPayments(response.data);
       } catch (error) {
