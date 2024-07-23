2392079501e5a2498230deadade1ccbc6ceb3c5c
DiazRock <corolariodiaz@gmail.com>
2024-07-24 00:56:24 +0300

    Using redux instead

--- apps/frontend/src/components/HomeComponent.jsx
+++ apps/frontend/src/components/HomeComponent.jsx
@@ -1,39 +1,44 @@
 import InitialPage from './InitialPage';
-import { sessionIsActive } from '../services/apiService';
 import AdminDashboard from './AdminDashboard';
+import { useSelector } from 'react-redux';
 import LogOut from './LogOut';
 
 const HomeComponent = () => {
-    if (!sessionIsActive()) {
+  const jwtToken = useSelector((state) => state.loginReducer.jwtSession);
+  const canEdit = useSelector((state) => state.loginReducer.canEdit);
+  const canWrite = useSelector((state) => state.loginReducer.canWrite);
+  const userRole = useSelector((state) => state.loginReducer.userRole);
+  console.log('The jwtToken ', jwtToken, canEdit, canWrite, userRole);
+    if (jwtToken === '') {
       return (
         <>
           <InitialPage/>
         </>
       )
     }
-    const user_role = localStorage.getItem('user_role')
-    console.log('The user role is ', user_role);
-    if (user_role === "admin") {
+    console.log('The user role is ', userRole);
+    if (userRole === "admin") {
       console.log('The user is an admin');
-      const permissions = localStorage.getItem('permissions').split(',');
-      console.log('The permissions are ', permissions);
       return (
         <>
-          <AdminDashboard permissions ={permissions}/>
+          <AdminDashboard permissions ={[canEdit, canWrite]}/>
           <LogOut />
         </>
         
       )
     }
 
-    if (user_role == 'user') {
+    if (userRole === 'user') {
       return (
         <>
           <LogOut />
         </>
       )
-      
     }
+
+    return (
+      <h1>Welcome to the Home Page. No user role detected</h1>
+    )
 }
 
 export default HomeComponent;
