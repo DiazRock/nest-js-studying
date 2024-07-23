5ab20db93cdf2adc4bbbe40affe4c1c5e9765429
DiazRock <corolariodiaz@gmail.com>
2024-07-24 00:51:32 +0300

    Using different permissions

--- apps/frontend/src/components/AdminDashboard.jsx
+++ apps/frontend/src/components/AdminDashboard.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 import { UserPermissions } from "../utils/permissions";
 
 const AdminDashboard = ({permissions}) => {
-    console.log("Admin Dashboard");
+    console.log("Admin Dashboard", permissions);
     return (
         <nav>
             <ul className="nav-links">
 
-                {permissions.includes(UserPermissions.WRITE) &&
+                {permissions[1] &&
                 <li><Link to="/create-payment">Create Payment</Link></li> 
                 }
-                {permissions.includes(UserPermissions.WRITE) &&
+                {permissions[1] &&
                 <li><Link to="/create-user">Create User</Link></li>}
 
-                {permissions.includes(UserPermissions.READ) &&
+                {permissions[0] &&
                 <li><Link to="/list-users">List Users</Link></li>}
 
-                {permissions.includes(UserPermissions.READ) &&
+                {permissions[0] &&
                 <li><Link to="/list-payments">List Payments</Link></li>} 
                 
             </ul>
