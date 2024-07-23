6621f62833642490e233128509a1e9a8e4b827c7
DiazRock <corolariodiaz@gmail.com>
2024-07-24 00:57:17 +0300

    Added a react-redux

--- apps/frontend/src/components/ListUsers.jsx
+++ apps/frontend/src/components/ListUsers.jsx
@@ -1,15 +1,17 @@
 // src/components/ListUsers.js
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { listUsers } from '../services/apiService';
 import '../styles/List.css';
 
 const ListUsers = () => {
+  const jwtToken = useSelector((state) => state.loginReducer.jwtSession);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await listUsers();
+        const response = await listUsers(jwtToken);
         setUsers(response.data);
       } catch (error) {
         alert('Failed to fetch users');
