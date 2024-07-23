909102fe92fff5ca821cca635f1b11d3dde5fac4
DiazRock <corolariodiaz@gmail.com>
2024-07-24 00:50:40 +0300

    Added the provider for the redux store

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,17 +1,21 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import RoutesDefinitions from './components/RoutesDefinitions';
 import './styles/App.css';
 
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-          <RoutesDefinitions/>
-      </Router>
-    </div>
+    <Provider store = {store} >
+      <div className="container">
+        <Router>
+            <RoutesDefinitions/>
+        </Router>
+      </div>
+    </Provider>
   );
 };
 
